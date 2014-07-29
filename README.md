fed624bf97a86ee49f7a44821dfca755ac226007
Shraddha Pai <Shraddha.Pai@camh.ca>
2014-07-29 10:12:10 -0400

    README includes background, install, usage instructions. Main code includes checks for I/O parameters

--- README.md
+++ README.md
@@ -2,3 +2,38 @@ mergePeaks_resolve
 ==================
 
 A C++ parser to convert output of HOMER's mergePeaks into a table of registered peak intensities
+Copyright 2014 Shraddha Pai
+Please see LICENSE for details on usage restrictions.
+
+Motivation and background
+---------------------------
+HOMER is a third-party software written by Chris Benner and others, which is routinely used for analysis of data from the ChIP-SEQ assay.
+HOMER contains a utility, mergePeaks, which  "registers" peaks from multiple files part of the same experiment; for example, to find overlapping peaks for individual timepoints in a timecourse experiment. For details on mergePeaks see: http://homer.salk.edu/homer/ngs/mergePeaks.html
+mergePeaks output prints a comma-separated list of peak identifiers from each file, without providing the average peak intensity for the registered peaks.
+
+This utility, mergePeaks_resolve, takes the output from HOMER mergePeaks and returns a table with mean peak intensities from registered peaks.
+
+
+INSTALLING mergePeaks_resolve
+-----------------------------
+This project currently provides a compiled binary for x86-64bit architecture.
+To compile from source, you will need a GCC compiler and Boost libraries installed.
+The makefile is in the project root directory.
+Once you have the prerequsites, simply type
+
+make 
+
+in the project directory. That should be it.
+
+Binary and instructions for compilation on other platforms welcome.
+
+USING mergePeaks_resolve
+-------------------------
+Either create a symbolic link to the binary "mergePeaks_resolve" in the directory where you will use it,
+or put it in a directory on the search path.
+
+Examples
+----------
+see examples/example.sh
+
+
--- src/mergePeaks_resolve.cpp
+++ src/mergePeaks_resolve.cpp
@@ -25,8 +25,8 @@
 using namespace std;
 
 static void show_usage(std::string name) {
-		std::cerr << "Usage: " << name << " <options>"
-		<< "Options:\n" 
+    std::cerr << "Usage: " << name << " <options>" << endl;
+    std::cerr << "Options:\n" 
 		<< "\t-h\tShow this usage message\n"
 		<< "\t-i\tfiles that were merged using the HOMER mergePeaks call. Order of input files must match that of column ordering in mergedFile\n"
 		<< "\t-m\toutput of HOMER mergePeaks call, a file with the merged peaks ID\n"
@@ -56,7 +56,8 @@ public:
         char line[MAX_LINELEN];
         vector <string> t; 
         
-		// ID -> peak size map
+        // create lookup table of peak ID -> size for corresponding input file
+        // ID -> peak size map
 		iFile.open(inFile.c_str()); // file has no header so we can just get to it.
 		std::map <string,double> id2peak_map;
         time_t start=time(0);
@@ -67,25 +68,20 @@ public:
 					boost::split(t,line,boost::is_any_of("\t"));
 					id2peak_map[t[3]] = atof(t[4].c_str()); 
                 i_ctr++;
-               //if (i_ctr % 1000000==0) cout << "." << endl;
-			}
+            }
 		}
 		iFile.close();
         time_t end=time(0);
-        cout << myCol << ": Input map Time = " << difftime(end,start) << " seconds" << endl;
+        cout << myCol << ": Time to make input map= " << difftime(end,start) << " seconds" << endl;
         
-      //  cout <<"Looking for offending entry:" << endl;
-       // cout << id2peak_map.count("1614_2021_34") << endl;
-      //  cout << "past it" << endl;
-        
-        
+        // write genomic coordinates if flag is set.
         if (coordFile.compare("")!=0) {
             cFile.open(coordFile.c_str());
             cFile << "chrom" << "\t" << "start" << "\t" << "end" << endl;
         } 
         
-        cout << "* Reading mergedFile: " << m << endl;
         // now we read the merged peak file and map back peak ids to intensities
+        cout << "* Reading mergedFile: " << m << endl;
         mFile.open(m.c_str()); oFile.open(outFile.c_str());
 		mFile.getline(line,MAX_LINELEN); 
         boost::split(t,line,boost::is_any_of("\t"));        
@@ -94,10 +90,14 @@ public:
         unsigned ctr; ctr=2;
         vector<string> missingKeys;
 		while (mFile.good()) {
-          //  cout << myCol-7 << ":" << ctr << ": about to get line" << endl;
+#ifdef DEBUG
+            cout << myCol-7 << ":" << ctr << ": about to get line" << endl;
+#endif
 			mFile.getline(line,MAX_LINELEN);
-          // cout << myCol-7 << ":" << ctr << ": got line" << endl;
-		  // cout << line << endl;
+#ifdef DEBUG
+           cout << myCol-7 << ":" << ctr << ": got line" << endl;
+		   cout << line << endl;
+#endif
 			if (std::strcmp(line,"")!=0){
 			boost::split(t,line,boost::is_any_of("\t"));
                 
@@ -107,38 +107,51 @@ public:
 			if (ctr % 1000 == 0) cout << "K";
 			if (ctr % 10000 ==0) cout << endl;
                 
-             //   cout << ctr << ": " << t[myCol] << endl;
-			vector<string> t2; boost::split(t2,t[myCol],boost::is_any_of(","));
-			double sum; sum=0;
-                for (unsigned i=0; i < t2.size(); i++) {
-                //    cout << "\tLooking for **" << t2[i] << "**" << endl;
-                    unsigned keyct = id2peak_map.count(t2[i]);
-                    if (keyct==0) 
-                        missingKeys.push_back(t2[i]);
-                    else {
+#ifdef DEBUG   
+                cout << ctr << ": " << t[myCol] << endl;
+#endif			
+            // split IDs in column to be processed
+                if (t[myCol].compare("")==0) {
+                    oFile << "NA" << endl;
+                } else {
+                    vector<string> t2; boost::split(t2,t[myCol],boost::is_any_of(","));
+                    double sum; sum=0;
+                    for (unsigned i=0; i < t2.size(); i++) {
+#ifdef DEBUG                        
+                    cout << "\tLooking for **" << t2[i] << "**" << endl;
+#endif
+                        unsigned keyct = id2peak_map.count(t2[i]);
+                        if (keyct==0) {
+                            missingKeys.push_back(t2[i]);
+#ifdef DEBUG                            
+                            cout << "Missing key" << t2[i] << endl;
+                            cout << "Full entry was **" << t[myCol] << "**" << endl;
+#endif                            
+                        } else {
                         map<string,double>::iterator it=id2peak_map.find(t2[i]);
                         sum+= it->second;
-                    };
+                        };
                 };
-				if (t2.size() > 0) {
-              //  cout << ctr <<": computing mean" << endl;
-              //  cout << ctr << sum << "/" << t2.size() << endl;
-            	oFile << sum/((double)t2.size()) << endl;;
-				} else 
-						oFile << "NA" << endl;
-           //     cout << myCol-7 <<":" << ctr << "done!" << endl;
-			}
+                    if (t2.size() > 0) {
+                    oFile << sum/((double)t2.size()) << endl;;
+                    } else {
+                    oFile << "NA" << endl;
+                    }
+                };
+            }
             ctr++;
             }
+#ifdef DEBUG        
         cout << myCol-7 << ": closefile" << endl;
+#endif
 		mFile.close();
         oFile.close();
         if (coordFile.compare("")!=0)
             cFile.close();
-        cout << "File " << myCol-7 << ": " << ctr << " records" << endl;
-        for (unsigned i=0; i < missingKeys.size(); i++) {
-            cout << myCol-7 << ": missing key=" << missingKeys[i] << endl;
-        }
+            cout << "File " << myCol-7 << ": " << ctr << " records" << endl;
+            for (unsigned i=0; i < missingKeys.size(); i++) {
+                cout << myCol-7 << ": missing key=" << missingKeys[i] << endl;
+            }
 		}
 		
 private:
@@ -154,9 +167,15 @@ private:
  Main function 
  ************************************************ */
 int main (int argc, char* argv[]) {
-char* inDir;
-char* mergedFile;
-char* outFile;
+    char* inDir; inDir=NULL;
+    char* mergedFile; mergedFile=NULL;
+    char* outFile; outFile=NULL;
+    
+    if (argc < 4) {
+        cerr << "Insufficient arguments" << endl;
+        show_usage(argv[0]);
+        return -1;
+    }
 
 // parse command-line args
 for (int i = 1; i < argc; ++i) {
@@ -176,8 +195,25 @@ for (int i = 1; i < argc; ++i) {
 	} else {
 		cerr << "Invalid command. Please see usage " << endl;
 		show_usage(argv[0]);
+        return -1;
 	}
 }
+    
+// I/O test
+    if (inDir==NULL) {
+        cerr << "-i not specified" << endl;
+        show_usage(argv[0]);
+        return -1;
+    } else if (mergedFile==NULL) {
+        cerr << "-m not specified" << endl;
+        show_usage(argv[0]);
+        return -1;
+    } else if (outFile==NULL) {
+        cerr << "-o not specified" << endl;
+                show_usage(argv[0]);
+        return -1;
+    }
+    
 
 // TODO : should test if any of these is empty
 
@@ -212,12 +248,12 @@ std::vector<boost::thread *> wvec;
             strcat(oWriteCmd, coordFile); strcat(oWriteCmd," ");
             strcat(delCmd, coordFile); strcat(delCmd, " ");
         } 
-        /*
+#ifdef DEBUG
         cout << "********" << i <<  "***********" << endl;
         cout << "val: " << t[i] << endl;
         cout << "I:" << ifile << endl;
         cout << "O:" << ofile << endl;
-        */
+#endif
 
         strcat(oWriteCmd, ofile); strcat(oWriteCmd," ");
         strcat(delCmd,ofile); strcat(delCmd, " ");
@@ -237,11 +273,15 @@ std::vector<boost::thread *> wvec;
     
     // finally merge output.
     strcat(oWriteCmd, "> "); strcat(oWriteCmd, outFile);
+#ifdef DEBUG
     cout << oWriteCmd << endl;
+#endif
     system(oWriteCmd);
+#ifdef DEBUG    
     cout << "Delete command: " << endl;
     cout << delCmd << endl;
-    //system(delCmd); // delete temporary files
+#endif
+    system(delCmd); // delete temporary files
 
 cout << "done" << endl;
 
